cbafd1d5374d5cb0e3af410f5f9f474296b6e8d8
Pierre Burel <contact@pierreburel.com>
2026-01-08 19:51:30 -0500

    Add Vue

---
+++ src/components/VueComponent.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+type Props = {
+  title?: string;
+  variant?: "a" | "b";
+};
+
+const props = withDefaults(defineProps<Props>(), {
+  variant: "a",
+});
+</script>
+
+<template>
+  <div :data-variant="props.variant">
+    <h2 v-if="props.title">{{ props.title }}</h2>
+
+    <slot />
+  </div>
+</template>
